b91ef1758ae0ab42108871f9958101d1dca03080
lazaa32 <ad.laza32@gmail.com>
2020-09-11 16:24:18 +0200

    Use blend

--- add_opacity_by_zoom.py
+++ add_opacity_by_zoom.py
@@ -32,7 +32,7 @@ total_processed = 0
 for arg in range(alpha_arg, len(sys.argv)):
     zoom_processed = 0
     zoom, zoom_alpha = sys.argv[arg].split(':')
-    alpha = int(float(zoom_alpha)*255)
+    alpha = float(zoom_alpha)
 
     # Get number of tiles on given zoom
     read_cur.execute("""
@@ -57,7 +57,7 @@ for arg in range(alpha_arg, len(sys.argv)):
     """, (zoom,))
 
     print('Updating Zoom {} with alpha value: {}'.format(
-        zoom, zoom_alpha))
+        zoom, alpha))
     sys.stdout.flush()
 
     for in_row in read_cur:
@@ -68,12 +68,11 @@ for arg in range(alpha_arg, len(sys.argv)):
         zoom_processed += 1
 
         in_im = Image.open(io.BytesIO(in_row[1]))
-        in_im.putalpha(alpha)
 
         stream = io.BytesIO()
         bg = Image.new("RGBA", in_im.size, background_color)
-        bg.paste(in_im, mask=in_im.split()[3])
-        bg.save(stream, format="PNG")
+        blended_im = Image.blend(bg, in_im, alpha)
+        blended_im.save(stream, format="PNG")
 
         write_cur.execute("""
             UPDATE images
